0be795a90c0f7fcefaeaf505563a9548fd0c0ce3
Jackson Vanover <jacksonvanover@gmail.com>
2022-08-09 15:41:07 -0700

    updated matrix generation parameters

--- HPCCG/test_matrices/generate_matrices.m
+++ HPCCG/test_matrices/generate_matrices.m
@@ -1,12 +1,14 @@
-dim = 1000;
+dim = 2000;
 n_samples = 1024;
 
-cond_start = 10;
-cond_end = 10000;
+cond_start = 10^2;
+cond_end = 10^7;
+density = 0.1;
+generation_method = 2;
 cond_space = linspace(cond_start, cond_end, n_samples);
 
 for k = 1:numel(cond_space)
-    A = sprandsym(dim, 0.2, 1/cond_space(k), 2);
+    A = sprandsym(dim, density, 1/cond_space(k), generation_method);    
     condition_number = condest(A);
     dm2hb(sprintf('A_%06d_%d.dat', k, condition_number), A);
 end
