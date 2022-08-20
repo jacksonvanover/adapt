3a862bbb247530140ce51a5ef6cb6ff9e629ece5
Jackson Vanover <jacksonvanover@gmail.com>
2022-08-19 17:54:58 -0700

    added read for matrix condition numbers in matlab file

--- HPCCG/test_matrices/generate_matrices.m
+++ HPCCG/test_matrices/generate_matrices.m
@@ -1,11 +1,7 @@
 dim = 200;
-n_samples = 1;
-
-cond_start = 10^2;
-cond_end = 10^7;
 density = 0.1;
 generation_method = 2;
-cond_space = linspace(cond_start, cond_end, n_samples);
+cond_space = readmatrix("./cond_space.txt");
 
 for k = 1:numel(cond_space)
     A = sprandsym(dim, density, 1/cond_space(k), generation_method);    
