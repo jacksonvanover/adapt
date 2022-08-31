e58921a614bf72a77a7d2ef1243a88abc4379dd6
Jackson Vanover <jacksonvanover@gmail.com>
2022-08-31 08:00:29 -0700

    changed default matrix generation method in matlab and removed condition number estimation

--- HPCCG/test_matrices/generate_matrices.m
+++ HPCCG/test_matrices/generate_matrices.m
@@ -1,10 +1,9 @@
 dim = 200;
 density = 0.1;
-generation_method = 2;
+generation_method = 1;
 cond_space = readmatrix("./cond_space.txt");
 
 for k = 1:numel(cond_space)
     A = sprandsym(dim, density, 1/cond_space(k), generation_method);    
-    condition_number = condest(A);
-    dm2hb(sprintf('./A_%06d_%d.dat', k, condition_number), A);
+    dm2hb(sprintf('./A_%06d_%d.dat', k, cond_space(k)), A);
 end
