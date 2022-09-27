1c20e22904b624cfe7f9549c4cf3d3c496c2779c
Jackson Vanover <jacksonvanover@gmail.com>
2022-09-27 11:40:37 -0700

    added conditional for parallel computing in matlab script, dependent on license availability

--- HPCCG/test_matrices/generate_matrices.m
+++ HPCCG/test_matrices/generate_matrices.m
@@ -3,9 +3,16 @@ density = 0.1;
 generation_method = 1;
 cond_space = readmatrix("./cond_space.txt");
 
-p = parpool;
+if license('checkout','Distrib_Computing_Toolbox') == 0
+    for k = 1:numel(cond_space)
+        A = sprandsym(dim, density, 1/cond_space(k), generation_method);    
+        dm2hb(sprintf('./A_%06d_%d.dat', k, cond_space(k)), A);
+    end
+else
+    p = parpool;
 
-parfor k = 1:numel(cond_space)
-    A = sprandsym(dim, density, 1/cond_space(k), generation_method);    
-    dm2hb(sprintf('./A_%06d_%d.dat', k, cond_space(k)), A);
+    parfor k = 1:numel(cond_space)
+        A = sprandsym(dim, density, 1/cond_space(k), generation_method);    
+        dm2hb(sprintf('./A_%06d_%d.dat', k, cond_space(k)), A);
+    end
 end
